be934f62c6e976185e855045cd4bbeb51cbc5d1c
chera <chernikoviam@gmail.com>
2021-10-17 16:23:22 +0300

    Fixed bug with calculation after equality. THE LAST COMMIT

--- js/script.js
+++ js/script.js
@@ -3,26 +3,16 @@ const inputCalc = document.querySelector(".account");
 const inputResult = document.querySelector(".result");
 
 const ONLY_NUMBERS = /^\d+$/;
+let wasEqual = false;
 
-const input = function (i) {
-    if (inputCalc.value === "Dividing by zero is not allowed") {
-        inputCalc.value = "";
-    }
-    if (inputCalc.value == 0) {
-        inputCalc.value = "";
-    }
-    inputCalc.value += i;
-};
-const backspace = function () {
-    inputCalc.value = inputCalc.value.substring(0, inputCalc.value.length - 1);
-};
-const clean = function () {
-    inputCalc.value = "";
-    inputResult.value = "";
-};
-const result = function () {
-    inputCalc.value = eval(inputCalc.value);
+const calcEqualResult = function () {
     inputResult.value = inputCalc.value;
+};
+
+const result = function () {
+    wasEqual = true;
+    inputCalc.value = eval(inputCalc.value);
+    calcEqualResult();
 
     if (inputCalc.value == Infinity) {
         inputCalc.value = "Dividing by zero is not allowed";
@@ -33,9 +23,29 @@ const result = function () {
         inputResult.value = "";
     }
 };
+
+const input = function (i) {
+    if (wasEqual === true) {
+        inputCalc.value = "";
+        inputResult.value = "";
+    }
+    if (inputCalc.value === "Dividing by zero is not allowed") {
+        inputCalc.value = "";
+    }
+    wasEqual = false;
+    inputCalc.value += i;
+};
+const backspace = function () {
+    inputCalc.value = inputCalc.value.substring(0, inputCalc.value.length - 1);
+};
+const clean = function () {
+    inputCalc.value = "";
+    inputResult.value = "";
+};
+
 const percent = function () {
     inputCalc.value = eval(inputCalc.value) / 100;
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 //Degrees
 const toRadians = function (deg) {
@@ -43,7 +53,7 @@ const toRadians = function (deg) {
 };
 const sin = function () {
     inputCalc.value = Math.sin(toRadians(eval(inputCalc.value)));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const cos = function () {
     if (inputCalc.value === "90") {
@@ -51,48 +61,49 @@ const cos = function () {
     } else {
         inputCalc.value = Math.cos(toRadians(eval(inputCalc.value)));
     }
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const rad = function () {
     inputCalc.value = toRadians(eval(inputCalc.value));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const tan = function () {
     inputCalc.value =
         Math.sin(toRadians(eval(inputCalc.value))) /
         Math.cos(toRadians(eval(inputCalc.value)));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const ctg = function () {
     inputCalc.value =
         Math.cos(toRadians(eval(inputCalc.value))) /
         Math.sin(toRadians(eval(inputCalc.value)));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 //Power functions
 const sqr = function () {
     inputCalc.value = eval(inputCalc.value) ** 2;
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const xInMinusOne = function () {
     inputCalc.value = eval(inputCalc.value) ** -1;
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const sqrt = function () {
     inputCalc.value = Math.sqrt(eval(inputCalc.value));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const lg = function () {
     inputCalc.value = Math.log10(eval(inputCalc.value));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const ln = function () {
     inputCalc.value = Math.log(eval(inputCalc.value));
-    inputResult.value = inputCalc.value;
+    calcEqualResult();
 };
 const factorial = function () {
     for (let i = +eval(inputCalc.value) - 1; i >= 1; i--) {
         inputCalc.value *= i;
     }
-    inputResult.value = inputCalc.value;
+    wasEqual = true;
+    calcEqualResult();
 };
